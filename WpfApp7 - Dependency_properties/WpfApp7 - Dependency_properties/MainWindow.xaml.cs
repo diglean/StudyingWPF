71773a1db31674ad16b3c39a1f7eb818a6071cf4
Diglean <diegoleandro2002@gmail.com>
2021-11-22 16:29:59 -0300

    Creating my own Dependency Property

--- WpfApp7 - Dependency_properties/WpfApp7 - Dependency_properties/MainWindow.xaml.cs
+++ WpfApp7 - Dependency_properties/WpfApp7 - Dependency_properties/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp7___Dependency_properties
 {
@@ -23,7 +24,36 @@ namespace WpfApp7___Dependency_properties
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal,
+                                                        delegate
+                                                        {
+                                                            int newvalue = 0;
+                                                            if (Counter == int.MaxValue)
+                                                            {
+                                                                newvalue = 0;
+                                                            }
+                                                            else
+                                                            {
+                                                                newvalue = Counter +1;
+                                                            }
+                                                            SetValue(CounterProperty, newvalue);
+                                                        }, Dispatcher);
         }
+
+
+
+        public int Counter
+        {
+            get { return (int)GetValue(CounterProperty); }
+            set { SetValue(CounterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Counter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CounterProperty =
+            DependencyProperty.Register("Counter", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+
 
     }
 }
