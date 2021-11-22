f6f68302de6cf08223a87dc1a4281a902579df69
Diglean <diegoleandro2002@gmail.com>
2021-11-22 13:44:32 -0300

    Understanding logical tree and visual tree

---
+++ WpfApp5 - Logical_Tree_and_Visual_Tree/WpfApp5 - Logical_Tree_and_Visual_Tree/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp5___Logical_Tree_and_Visual_Tree
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
