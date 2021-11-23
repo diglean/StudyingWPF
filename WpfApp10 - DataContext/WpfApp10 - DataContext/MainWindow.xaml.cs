31c7ed362114a5a4d836e5cf1450fd39bd72010a
Diglean <diegoleandro2002@gmail.com>
2021-11-23 10:35:59 -0300

    Understanding DataContext

---
+++ WpfApp10 - DataContext/WpfApp10 - DataContext/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10___DataContext
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new Person()
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                Age = 30
+            };
+
+            //DataContext is saved into obj
+        }
+    }
+}
